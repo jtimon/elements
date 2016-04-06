0fe6e8f9488bd63199a5eed8ecca706818ebf0c2
instagibbs <gsanders87@gmail.com>
2016-04-06 10:26:47 -0700

    DOWN: FIXME: Unit tests compile and pass

--- src/test/versionbits_tests.cpp
+++ src/test/versionbits_tests.cpp
@@ -187,8 +187,8 @@ BOOST_AUTO_TEST_CASE(versionbits_test)
     }
 
     // Sanity checks of version bit deployments
-    std::unique_ptr<CChainParams> mainnetParamsCtx = CChainParams::Factory(CBaseChainParams::MAIN);
-    const Consensus::Params& mainnetParams = mainnetParamsCtx.GetConsensus();
+    std::unique_ptr<CChainParams> mainnetParamsCtx = std::unique_ptr<CChainParams>(CChainParams::Factory(CBaseChainParams::MAIN));
+    const Consensus::Params& mainnetParams = mainnetParamsCtx->GetConsensus();
     for (int i=0; i<(int) Consensus::MAX_VERSION_BITS_DEPLOYMENTS; i++) {
         uint32_t bitmask = VersionBitsMask(mainnetParams, (Consensus::DeploymentPos)i);
         // Make sure that no deployment tries to set an invalid bit.
@@ -214,7 +214,7 @@ BOOST_AUTO_TEST_CASE(versionbits_computeblockversion)
 {
     // Check that ComputeBlockVersion will set the appropriate bit correctly
     // on mainnet.
-    const Consensus::Params &mainnetParams = Params(CBaseChainParams::MAIN).GetConsensus();
+    const Consensus::Params &mainnetParams = CChainParams::Factory(CBaseChainParams::MAIN)->GetConsensus();//Params(CBaseChainParams::MAIN).GetConsensus();
 
     // Use the TESTDUMMY deployment for testing purposes.
     int64_t bit = mainnetParams.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit;
