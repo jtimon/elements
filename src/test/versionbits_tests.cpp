3e2fdc6163d209bef68777035251c5f6ffcc82cf
Jorge Timón <jtimon@jtimon.cc>
2016-09-23 01:22:50 +0200

    Preparations: Pass mapArgs to CChainParams::Factory

--- src/test/versionbits_tests.cpp
+++ src/test/versionbits_tests.cpp
@@ -187,7 +187,8 @@ BOOST_AUTO_TEST_CASE(versionbits_test)
     }
 
     // Sanity checks of version bit deployments
-    std::unique_ptr<CChainParams> mainnetParamsCtx = std::unique_ptr<CChainParams>(CChainParams::Factory(CBaseChainParams::MAIN));
+    std::map<std::string, std::string> mapArgs;
+    std::unique_ptr<CChainParams> mainnetParamsCtx = std::unique_ptr<CChainParams>(CChainParams::Factory(CBaseChainParams::MAIN, mapArgs));
     const Consensus::Params& mainnetParams = mainnetParamsCtx->GetConsensus();
     for (int i=0; i<(int) Consensus::MAX_VERSION_BITS_DEPLOYMENTS; i++) {
         uint32_t bitmask = VersionBitsMask(mainnetParams, (Consensus::DeploymentPos)i);
@@ -214,7 +215,8 @@ BOOST_AUTO_TEST_CASE(versionbits_computeblockversion)
 {
     // Check that ComputeBlockVersion will set the appropriate bit correctly
     // on mainnet.
-    const Consensus::Params &mainnetParams = CChainParams::Factory(CBaseChainParams::MAIN)->GetConsensus();//Params(CBaseChainParams::MAIN).GetConsensus();
+    std::map<std::string, std::string> mapArgs;
+    const Consensus::Params &mainnetParams = CChainParams::Factory(CBaseChainParams::MAIN, mapArgs)->GetConsensus();//Params(CBaseChainParams::MAIN).GetConsensus();
 
     // Use the TESTDUMMY deployment for testing purposes.
     int64_t bit = mainnetParams.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit;
