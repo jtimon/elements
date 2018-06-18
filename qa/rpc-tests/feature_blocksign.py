ac84ac54c59cec5c76a50dc986cfb4af01fa457c
Jorge Timón <jtimon@jtimon.cc>
2018-06-19 00:29:32 +0200

    f'QA: Introduce feature_blocksign.py'

--- qa/rpc-tests/feature_blocksign.py
+++ qa/rpc-tests/feature_blocksign.py
@@ -4,19 +4,18 @@
 import os
 import random
 import hashlib
-from test_framework.address import byte_to_base58
-from test_framework.key import CECKey
-from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import (
-    assert_equal,
-    connect_nodes_bi,
-    start_nodes,
+
+from test_framework import (
+    address,
+    key,
+    test_framework,
+    util,
 )
 
 # Generate wallet import format from private key.
 def wif(pk):
     # Base58Check version for regtest WIF keys is 0xef = 239
-    return byte_to_base58(pk, 239)
+    return address.byte_to_base58(pk, 239)
 
 # The signblockscript is a Bitcoin Script k-of-n multisig script.
 def make_signblockscript(num_nodes, required_signers, keys):
@@ -31,14 +30,14 @@ def make_signblockscript(num_nodes, required_signers, keys):
     print('signblockscript', script)
     return script
 
-class BlockSignTest(BitcoinTestFramework):
+class BlockSignTest(test_framework.BitcoinTestFramework):
 
     # Dynamically generate N keys to be used for block signing.
     def init_keys(self, num_keys):
         self.keys = []
         self.wifs = []
         for i in range(num_keys):
-            k = CECKey()
+            k = key.CECKey()
             pk_bytes = hashlib.sha256(str(random.getrandbits(256)).encode('utf-8')).digest()
             k.set_secretbytes(pk_bytes)
             w = wif(pk_bytes)
@@ -64,14 +63,14 @@ class BlockSignTest(BitcoinTestFramework):
         ]] * self.num_nodes
 
     def setup_network(self, split=False):
-        self.nodes = start_nodes(self.num_nodes, self.options.tmpdir, self.extra_args)
+        self.nodes = util.start_nodes(self.num_nodes, self.options.tmpdir, self.extra_args)
         # Have every node import its block signing private key.
         for i in range(self.num_nodes):
             self.nodes[i].importprivkey(self.wifs[i])
             if i + 1 < self.num_nodes:
-                connect_nodes_bi(self.nodes, i, i + 1)
+                util.connect_nodes_bi(self.nodes, i, i + 1)
             else:
-                connect_nodes_bi(self.nodes, 0, i)
+                util.connect_nodes_bi(self.nodes, 0, i)
         self.is_network_split = False
         self.sync_all()
 
@@ -89,12 +88,12 @@ class BlockSignTest(BitcoinTestFramework):
         sigs = []
         for i in range(self.required_signers):
             result = miner.combineblocksigs(block, sigs)
-            assert_equal(result["complete"], False)
+            util.assert_equal(result["complete"], False)
             sigs.append(self.nodes[i].signblock(block))
 
         # miner submits
         result = miner.combineblocksigs(block, sigs)
-        assert_equal(result["complete"], True)
+        util.assert_equal(result["complete"], True)
         signedblock = result["hex"]
         miner.submitblock(signedblock)
 
@@ -105,7 +104,7 @@ class BlockSignTest(BitcoinTestFramework):
 
     def check_height(self, expected_height):
         for n in self.nodes:
-            assert_equal(n.getblockcount(), expected_height)
+            util.assert_equal(n.getblockcount(), expected_height)
 
     def run_test(self):
         self.check_height(0)
